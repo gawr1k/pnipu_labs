d38b37d19ce3ae3b7a7d10cd6717067d4eaad4a2
gawr1k <gasimmurasov@gmail.com>
2022-11-17 00:46:25 +0500

    two cpmmit

---
+++ home/home/HelloWorld.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class HelloWorld {
+    public static int Main(string[] args) {
+        Console.WriteLine("введите слово");
+        string word = Console.ReadLine();
+        if (args.Length == 1) {
+            int N = Int32.Parse(args[0]);
+            int num = 20;
+            for (int i = 1; i <=N; i++) {
+                num += 2 / 2;
+            }Console.WriteLine(num);
+            return 0;
+        }
+        else {
+            Console.WriteLine("No N provided!");
+            return 1;
+        }
+    }
+}
+
+
+
